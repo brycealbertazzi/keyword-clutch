5b92e143cf0e39e18759632c4255f944c706aba9
brycealbertazzi <brycealbertazzi@gmail.com>
2024-02-24 18:04:35 -0700

    Get new logo

--- client-side/src/components/Footer.js
+++ client-side/src/components/Footer.js
@@ -1,46 +1,41 @@
 import React from 'react'
 import './Footer.css'
-import RankRocketLogo from '../rank-rocket-logo.png'
-import RankRocketLogoWhite from '../rank-rocket-logo-white.png'
-import RankRocketLogoRed from '../rank-rocket-logo-red.png'
-import RankRocketLogoBlue from '../rank-rocket-logo-blue.png'
-import RankRocketLogoGreen from '../rank-rocket-logo-green.png'
-
+import KeywordClutchLogo from '../keyword-clutch.png'
 
 export const Footer = () => {
 
     return (
         <footer className='footer-container'>
             <div>
-                <img width={75} height={100} src={RankRocketLogo} alt='Rank Rocket Logo' />
+                <img width={100} height={100} src={KeywordClutchLogo} alt='Rank Rocket Logo' />
             </div>
             <div>
-                <img width={75} height={100} src={RankRocketLogoWhite} alt='Rank Rocket Logo' />
+                <img width={100} height={100} src={KeywordClutchLogo} alt='Rank Rocket Logo' />
             </div>
             <div>
-                <img width={75} height={100} src={RankRocketLogoRed} alt='Rank Rocket Logo' />
+                <img width={100} height={100} src={KeywordClutchLogo} alt='Rank Rocket Logo' />
             </div>
             <div>
-                <img width={75} height={100} src={RankRocketLogoBlue} alt='Rank Rocket Logo' />
+                <img width={100} height={100} src={KeywordClutchLogo} alt='Rank Rocket Logo' />
             </div>
             <div>
-                <img width={75} height={100} src={RankRocketLogoGreen} alt='Rank Rocket Logo' />
+                <img width={100} height={100} src={KeywordClutchLogo} alt='Rank Rocket Logo' />
             </div>
             <p>© 2024 Rank Rocket API</p>
             <div>
-                <img width={75} height={100} src={RankRocketLogo} alt='Rank Rocket Logo' />
+                <img width={100} height={100} src={KeywordClutchLogo} alt='Rank Rocket Logo' />
             </div>
             <div>
-                <img width={75} height={100} src={RankRocketLogoWhite} alt='Rank Rocket Logo' />
+                <img width={100} height={100} src={KeywordClutchLogo} alt='Rank Rocket Logo' />
             </div>
             <div>
-                <img width={75} height={100} src={RankRocketLogoRed} alt='Rank Rocket Logo' />
+                <img width={100} height={100} src={KeywordClutchLogo} alt='Rank Rocket Logo' />
             </div>
             <div>
-                <img width={75} height={100} src={RankRocketLogoBlue} alt='Rank Rocket Logo' />
+                <img width={100} height={100} src={KeywordClutchLogo} alt='Rank Rocket Logo' />
             </div>
             <div>
-                <img width={75} height={100} src={RankRocketLogoGreen} alt='Rank Rocket Logo' />
+                <img width={100} height={100} src={KeywordClutchLogo} alt='Rank Rocket Logo' />
             </div>
         </footer>
     )
--- client-side/src/components/Navbar.js
+++ client-side/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Navbar.css'
 import '../App.css'
-import RankRocketLogo from '../rank-rocket-logo-white.png'
+import KeywordClutchLogo from '../keyword-clutch.png'
 import { useNavigate } from 'react-router-dom'
 
 export const Navbar = () => {
@@ -9,7 +9,7 @@ export const Navbar = () => {
 
     return (
         <div className='nav-container'>
-            <img width={75} height={100} src={RankRocketLogo} alt='Rank Rocket Logo'/>
+            <img width={100} height={100} src={KeywordClutchLogo} alt='Rank Rocket Logo'/>
             <h1>KEYWORD CLUTCH</h1>
             <div className='nav-right'>
                 <button className='app-button' onClick={() => navigate('/home')}>Sign Up</button>
