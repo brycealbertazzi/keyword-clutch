9cdbe557ccac63dc3381f058911c073de08c905b
brycealbertazzi <brycealbertazzi@gmail.com>
2024-02-26 15:31:33 -0700

    add webpack file

---
+++ client-side/webpack.config.js
@@ -0,0 +1,24 @@
+import path from 'path';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-private-property-in-object']
+          }
+        }
+      }
+    ]
+  }
+}
