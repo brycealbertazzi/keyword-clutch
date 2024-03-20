3e8fee3c74b1f09b0b6d7a24d4d5431ed671e677
brycealbertazzi <brycealbertazzi@gmail.com>
2024-03-20 17:44:21 -0600

    Resolve config vars

--- client-side/src/components/stripe/StripeContainer.js
+++ client-side/src/components/stripe/StripeContainer.js
@@ -3,12 +3,10 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { PaymentForm } from './PaymentForm';
 
-// const PUBLISHABLE_STRIPE_KEY = 'pk_live_51OrnRKF65CKVrg3s7xlfQaJDQ5OFTI0DUoJQJoozX78rnd5MLhRCpHkacksdbDbS41S0YoiQwKsj7FYUxtHGfR6E003FR3LD7d'
-const PUBLISHABLE_STRIPE_TEST_KEY = 'pk_test_51OrnRKF65CKVrg3sgDM5OaRidvjNukAa9JV1XCahoaJwOnoVu9Lz0hEdP5m6We1lFSO5KfNMxXYZuDzRuuBrmlsv00g6HcNuMp'
-const stripePromise = loadStripe(PUBLISHABLE_STRIPE_TEST_KEY)
+const PUBLISHABLE_KEY = process.env.REACT_APP_PUBLISHABLE_STRIPE_TEST_KEY
+const stripePromise = loadStripe(PUBLISHABLE_KEY)
 
 export const StripeContainer = () => {
-
     return (
         <Elements stripe={stripePromise}>
             <PaymentForm />
